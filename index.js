8a3331553800bf7f19c88c70ce74ca1c49cc8392
Marcel Wójcik <marcelw.dev@gmail.com>
2026-02-17 21:47:08 +0100

    After Stage 2

--- index.js
+++ index.js
@@ -4,16 +4,43 @@ const getRandomDog = async () => {
     const src = await json.message;
     imgSrc = await src;
     image.src = await imgSrc;
+    content.innerHTML = `<img src="${imgSrc}" alt="dog">`;
 }
 
-let button = document.getElementById('button-random-dog');
+const getDogByBreed = async () => {
+    const breed = breedInput.value;
+    try {
+        const request = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+
+        if(request.status === 404) {
+            throw new Error("Breed not found!");
+        }
+        const json = await  request.json();
+        const src = await json.message;
+        imgSrc = await src;
+        image.src = await imgSrc;
+        content.innerHTML = `<img src="${imgSrc.toLowerCase()}" alt="dog">`;
+    } catch(e){
+
+        content.innerHTML = `<p>${e.message}</p>`;
+    }
+
+}
+
 let content = document.getElementById('content');
 let imgSrc = '';
+let randomButton = document.getElementById('button-random-dog');
+let breedButton = document.getElementById('button-show-breed');
+let breedInput = document.getElementById('input-breed');
 
 
 const image = document.createElement('img');
 content.append(image);
 
-button.addEventListener('click', getRandomDog);
+
+
+randomButton.addEventListener('click', getRandomDog);
+breedButton.addEventListener('click', getDogByBreed);
+
 
 
